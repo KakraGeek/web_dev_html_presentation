919a63d288429f8ee561411c295a1e5f6613a8ff
KakraGeek <desmond.asiedu@gmail.com>
2025-08-19 16:22:08 +0000

    fix: Use CDN versions of Reveal.js to resolve build and runtime issues

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   server: {
@@ -10,33 +9,11 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
-      input: 'index.html',
-      external: [
-        '/reveal.js/dist/reveal.esm.js',
-        '/reveal.js/plugin/markdown/markdown.esm.js',
-        '/reveal.js/plugin/notes/notes.esm.js',
-        '/reveal.js/plugin/highlight/highlight.esm.js',
-        '/reveal.js/plugin/search/search.esm.js',
-        '/reveal.js/plugin/zoom/zoom.esm.js'
-      ]
+      input: 'index.html'
     }
   },
   optimizeDeps: {
     include: ['reveal.js']
   },
-  publicDir: 'public',
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'node_modules/reveal.js/dist/*',
-          dest: 'reveal.js/dist'
-        },
-        {
-          src: 'node_modules/reveal.js/plugin/*',
-          dest: 'reveal.js/plugin'
-        }
-      ]
-    })
-  ]
+  publicDir: 'public'
 })
